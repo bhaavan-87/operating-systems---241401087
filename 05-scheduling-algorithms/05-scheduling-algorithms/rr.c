29d40a22e41735a273c8d5c85cdbe40a09c7157c
bhaavan-87 <saidattabhaavan.mbn.2024.csbs@rajalakshmi.edu.in>
2026-05-13 22:38:03 +0530

    Update rr.c

--- 05-scheduling-algorithms/05-scheduling-algorithms/rr.c
+++ 05-scheduling-algorithms/05-scheduling-algorithms/rr.c
@@ -1,1 +1,57 @@
+#include<stdio.h>
 
+int main()
+{
+    int i, n, tq, remain, count = 0;
+    int bt[10], rt[10];
+    int wt = 0, tat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    remain = n;
+
+    printf("Enter Burst Time:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &bt[i]);
+        rt[i] = bt[i];
+    }
+
+    printf("Enter Time Quantum: ");
+    scanf("%d", &tq);
+
+    i = 0;
+
+    while(remain != 0)
+    {
+        if(rt[i] <= tq && rt[i] > 0)
+        {
+            count += rt[i];
+            rt[i] = 0;
+            remain--;
+        }
+        else if(rt[i] > 0)
+        {
+            rt[i] -= tq;
+            count += tq;
+        }
+
+        if(rt[i] == 0)
+        {
+            tat += count;
+            wt += count - bt[i];
+        }
+
+        if(i == n-1)
+            i = 0;
+        else
+            i++;
+    }
+
+    printf("\nAverage Waiting Time = %f\n", (float)wt/n);
+    printf("Average Turnaround Time = %f\n", (float)tat/n);
+
+    return 0;
+}
