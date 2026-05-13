37d2493d068cbda54974d8d95646370ebb4abc8a
bhaavan-87 <saidattabhaavan.mbn.2024.csbs@rajalakshmi.edu.in>
2026-05-13 22:31:45 +0530

    Update sjf.c

--- 05-scheduling-algorithms/05-scheduling-algorithms/sjf.c
+++ 05-scheduling-algorithms/05-scheduling-algorithms/sjf.c
@@ -1,1 +1,73 @@
+#include<stdio.h>
 
+int main()
+{
+    int bt[20], p[20], pr[20], wt[20], tat[20];
+    int i, j, n, temp;
+    float avgwt = 0, avgtat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter Burst Time and Priority:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("P%d:\n", i+1);
+
+        printf("Burst Time: ");
+        scanf("%d", &bt[i]);
+
+        printf("Priority: ");
+        scanf("%d", &pr[i]);
+
+        p[i] = i+1;
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = i+1; j < n; j++)
+        {
+            if(pr[i] > pr[j])
+            {
+                temp = pr[i];
+                pr[i] = pr[j];
+                pr[j] = temp;
+
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
+    wt[0] = 0;
+
+    for(i = 1; i < n; i++)
+        wt[i] = wt[i-1] + bt[i-1];
+
+    for(i = 0; i < n; i++)
+    {
+        tat[i] = wt[i] + bt[i];
+
+        avgwt += wt[i];
+        avgtat += tat[i];
+    }
+
+    printf("\nProcess\tPriority\tBurst Time\tWaiting Time\tTurnaround Time\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n",
+               p[i], pr[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avgwt/n);
+    printf("\nAverage Turnaround Time = %.2f", avgtat/n);
+
+    return 0;
+}
